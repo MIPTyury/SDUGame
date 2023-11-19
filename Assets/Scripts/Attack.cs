084bf479633fb44abbd72d8157edc8ffa41058a8
Yury <fedin.iua@phystech.edu>
2023-11-19 22:03:59 +0300

    Пофиксил и обновил вращение и отслеживание мышки

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -20,11 +20,9 @@ namespace DefaultNamespace
 
         private void Update()
         {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
+            
                 Debug.Log("Space pressed");
                 RotationFollowingMouse();
-            }
         }
 
         private void RotationFollowingMouse()
@@ -32,9 +30,12 @@ namespace DefaultNamespace
             Vector2 screenPosition = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
             Vector2 worldPosition = Camera.main.ScreenToWorldPoint(screenPosition);
 
+            worldPosition.x -= player.position.x;
+            worldPosition.y -= player.position.y;
+            
             float angle = Mathf.Atan2(worldPosition.y, worldPosition.x) * Mathf.Rad2Deg;
 
-            Vector3 rot = new Vector3(0, 0, angle);
+            Vector3 rot = new Vector3(0, 0, angle - 90);
 
             Quaternion currRotation = new Quaternion();
             currRotation.eulerAngles = rot;
