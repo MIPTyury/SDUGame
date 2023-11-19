649ccde10e0e930fef0e4ba3726ab8dd1f1ff959
Yury <fedin.iua@phystech.edu>
2023-11-19 18:54:30 +0300

    начал делать ротацию

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,37 @@
+using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace DefaultNamespace
+{
+    public class Attack : MonoBehaviour
+    {
+        [SerializeField] private Transform player;
+        // Update is called once per frame
+
+
+        private void Awake()
+        {
+            if (!player)
+                player = FindObjectOfType<MovementControlls>().transform;
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Debug.Log("Space pressed");
+                RotationFollowingMouse();
+            }
+        }
+
+        private void RotationFollowingMouse()
+        {
+            Vector2 mousePos = Input.mousePosition;
+
+            float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+
+            player.rotation.Set(0, 0, -angle, 0);
+        }
+    }
+}
